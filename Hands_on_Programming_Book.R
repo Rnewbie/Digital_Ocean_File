67dc4016fc9fa8f282984c96d47d7491ed9b2396
Saw Simeon <doe_doe207@hotmail.com>
2015-05-18 11:54:37 -0400

    ok

--- Hands_on_Programming_Book.R
+++ Hands_on_Programming_Book.R
@@ -685,6 +685,74 @@ num <- 10000000
 print(num)
 class(num) <- c("POSIXct", "POSIXt")
 print(num)
+attributes(DECK)
+row.names(DECK)
+row.names(DECK) <- 101:152
+levels(DECK) <- c("level 1", "level 2", "level 3")
+attributes(DECK)
+one_play <- play()
+one_play
+attributes(one_play)
+attr(one_play, "symbols") <- c("B", "0", "B")
+attributes(one_play)
+attr(one_play, "symbols")
+one_play
+one_play  + 1
+play <- function() {
+  symbols <- get_symbols()
+  print(symbols)
+  scoe(symbols)
+}
+play <- function() {
+  symbols <- get_symbols()
+  prize <- score(symbols)
+  attr(prize, "symbols") <- symbols
+  prize
+}
+play()
+two_play <- play()
+two_play
+play <- function(){
+  symbols <- get_symbols()
+  structure
+}
+slot_display <- function(prize) {
+  # extract symbols
+  symbols ,- attr(prize, "symbols")
+  # collapse symbols into single string
+  symbols <- paste(symbols, collapse = "")
+  # combine symbol with prize as a regular expression
+  # |n is regular expression fro new line (i.e. return or enter)
+  string <- paste(symbols, prize, sep = "\n$")
+  cat(string)
+}
+slot_display(one_play)
+symbols <- attr(one_play, "symbols")
+symbols
+symbols <- paste(symbols, collapse =  "")
+prize <- one_play
+string <- paste(symbols, prize, sep = "\n$")
+string
+cat(string)
+slot_display(play())
+slot_display(play())
+print(pi)
+pi
+print(head(deck))
+head(deck)
+print(play())
+play()
+num <- 1000000
+print(num)
+class(num) <- c("POSIXct", "POSIXt")
+print(num)
+print
+print.POSIXct
+print.factor
+methods(print)
+### page 164
+
+
 
 
 
